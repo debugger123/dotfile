2fa6a71f331bba9b457deb59994cd9a977a1846f
bfwsusr <bfwsusr@sysmail>
2024-11-21 00:09:39 +0200

    Improve completions behavior, add rust support, vterm, and tweak monitor to ws table a bit

--- 1-asroot.sh
+++ 1-asroot.sh
@@ -1,16 +1,10 @@
-apt install -y git wget xorg xz-utils build-essential unclutter flameshot xsettingsd
+apt install -y git wget xorg xz-utils build-essential unclutter flameshot xsettingsd cmake
 
 apt install -y fonts-liberation2 fonts-dejavu materia-gtk-theme
 
 apt install -y xorg-dev libjansson-dev libtree-sitter-dev libmagickwand-dev libgtk-3-dev libwebkit2gtk-4.0-dev libgnutls28-dev libsystemd-dev libgif-dev libncurses-dev texinfo libgccjit-12-dev libm17n-dev libotf-dev libxft-dev
 
 apt install -y lxpolkit lxdm
-
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); print "\n"$0"\n"}'
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-apt update
-apt install firefox
 
 mkdir build
 cd build
--- 2-emacs.sh
+++ 2-emacs.sh
@@ -1,1 +1,2 @@
-emacs --batch --eval "(progn (require 'org) (org-babel-tangle-file \"emacs.org\") (load-file \"setup.el\") (org-babel-tangle-file \"emacs.org\"))"
+emacs --batch --eval "(progn (require 'org) (org-babel-tangle-file \"emacs.org\"))"
+emacs --batch --eval "(progn (require 'org) (load-file \"setup.el\") (org-babel-tangle-file \"emacs.org\"))"
